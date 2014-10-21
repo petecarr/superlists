a37dfcad402cb836ee6bbb421b90946516c8941d
petecarr <petecarr@yahoo.com>
2014-10-20 17:48:27 -0700

    Add list field.

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -14,9 +14,25 @@ class Migration(migrations.Migration):
             name='Item',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('text', models.TextField(default='')),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(default='', to='lists.List'),
+            preserve_default=True,
         ),
     ]
